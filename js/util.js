ba2171c5248d2c10366b7de9dc36471938ed538d
xsd <xuesongde@vread.cn>
2018-08-09 11:45:56 +0800

    xhr

---
+++ js/util.js
@@ -0,0 +1,23 @@
+function Util(){
+	this.getXmlHttpRequest=function(){
+		var xhr;
+		//非IE浏览器创建XmlHttpRequest对象
+		if(window.XMLHttpRequest){
+			xhr=new window.XMLHttpRequest();
+		}
+		//IE浏览器创建XmlHttpRequest对象
+		if(window.ActiveXObject){
+			try{
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}catch(e){
+				console.error(e)
+				try{
+					xhr = new ActiveXObject("msxml2.XMLHTTP");
+				}catch(er){
+					console.error(er)
+				}
+			}
+		}
+		return xhr;
+	}
+}
